187c5d12cf2e254f3ebc0f612d7b4cb87c8ebdba
FairyGreta <Christina_Art@mail.ru>
2022-07-21 16:40:43 +0500

    Добавлены тесты для класса Player

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,12 +1,19 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
+    //sumGenre: следует посчитать суммарное время одной игры
     @Test
-    public void shouldSumGenreIfOneGame() {
+    public void shouldCheckSumGenreIfOneGame() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
@@ -19,5 +26,166 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    //sumGenre: следует переустановить игру и проверить наигранное время
+    @Test
+    public void shouldAddDoubleGame() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+
+        Player player = new Player("Петя");
+        player.installGame(game1);
+        player.play(game1, 2);
+        player.installGame(game1);
+
+        int expected = 2;
+        int actual = player.sumGenre(game1.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    //sumGenre: следует посчитать суммарное время двух игр в одном жанре
+    @Test
+    public void shouldCheckSumGenreTwoGames() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 3);
+        player.play(game2, 2);
+        player.play(game3, 1);
+
+        int expected = 5;
+        int actual = player.sumGenre("Шутер");
+        assertEquals(expected, actual);
+    }
+
+    //sumGenre: следует посчитать суммарное время игры, если 0 часов наиграно
+    // тест проходит
+    @Test
+    public void shouldCheckSumGenreIfPlayZeroTime() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 0);
+        player.play(game2, 0);
+        player.play(game3, 0);
+
+        int expected = 0;
+        int actual = player.sumGenre(game1.getGenre());
+
+        assertEquals(expected, actual);
+    }
+
+    //play: следует проверить выпадение RuntimeException, если игра не установлена
+    //тест 1: ни одна игра не установлена
+    @Test
+    public void shouldCheckPlayWithoutInstall() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+
+        Player player = new Player("Petya");
+
+        assertThrows(RuntimeException.class, () ->
+                player.play(game2, 1)
+        );
+    }
+
+    // тест 2: установлены две игры из трех
+    @Test
+    public void shouldCheckThrowRunTimeException() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+
+
+        assertThrows(RuntimeException.class, () -> {
+            player.play(game3, 1);
+
+        });
+    }
+
+    //mostPlayerByGenre: следует вернуть жанр, в который играли больше всего
+    @Test
+    public void shouldCheckMostPlayerByGenre() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+        Game game4 = store.publishGame("Allods online", "MMORPG");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game3);
+        player.installGame(game4);
+        player.play(game1, 2);
+        player.play(game3, 4);
+        player.play(game4, 3);
+
+        Game expected = game3;
+        Game actual = player.mostPlayerByGenre("Файтинг");
+
+        assertEquals(expected, actual);
+    }
+
+    //mostPlayerByGenre: следует вернуть null, если в игры этого жанра не играли
+    //тест проходит
+    @Test
+    public void shouldCheckMostPlayerByGenreNoInstallGame() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+        Game game3 = store.publishGame("Mortal Kombat", "Файтинг");
+        Game game4 = store.publishGame("Allods online", "MMORPG");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.installGame(game4);
+        player.play(game1, 3);
+        player.play(game2, 2);
+        player.play(game3, 4);
+        player.play(game4, 1);
+
+
+        String expected = null;
+        Game actual = player.mostPlayerByGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+
+    //mostPlayerByGenre: следует вернуть null, если время отрицательное
+    @Test
+    public void shouldCheckMostPlayerByGenreWithГnacceptableTime() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Call of Dute", "Шутер");
+        Game game2 = store.publishGame("Apex Legends", "Шутер");
+
+        Player player = new Player("Petya");
+        player.installGame(game1);
+        player.play(game1, -1);
+
+        String expected = null;
+        Game actual = player.mostPlayerByGenre("Шутер");
+
+        assertEquals(expected, actual);
+    }
+
 }
